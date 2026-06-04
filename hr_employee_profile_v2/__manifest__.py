c1573284e3c4aa1ba30b96e994efc54c8c711909
janmerjacamo <160087981+janmerjacamo@users.noreply.github.com>
2026-06-03 22:08:02 -0600

    Bump version to 19.0.2 and add mail dependency
    Updated version and dependencies for HR Employee Profile V2.

--- hr_employee_profile_v2/__manifest__.py
+++ hr_employee_profile_v2/__manifest__.py
@@ -1,10 +1,15 @@
 {
     "name": "HR Employee Profile V2",
-    "version": "19.0.1.0.0",
-    "depends": ["hr"],
+    "version": "19.0.2.0.0",
+    "depends": ["hr", "mail"],  # mail para attachments quizá
     "data": [
-        "views/hr_employee_views.xml"
+        "security/ir.model.access.csv",
+        "views/hr_employee_views.xml",
+        "views/hr_employee_work_experience_views.xml",
+        "views/hr_employee_reference_views.xml",
+        "views/hr_employee_study_views.xml",
+        "views/hr_employee_technical_study_views.xml",
     ],
     "installable": True,
-    "application": False
+    "application": False,
 }
